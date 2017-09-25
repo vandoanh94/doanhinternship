bae44816d4cbf234521716626e540582bbbc0d57
ORIENTSOFTWARE\nvdoanh <vandoanh94@gmail.com>
2017-09-25 11:26:26 +0700

    UI

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -2,16 +2,15 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LearnComponent } from "./learn/learn.component";
 import { GuidedtourComponent } from "./guided tour/guided_tour.component";
+import { AppComponent } from './app.component';
 
 
  
 const routes: Routes = [
-  { path: '', redirectTo: '/learn', pathMatch: 'full' },
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: '#',  component: AppComponent },
   { path: 'learn',  component: LearnComponent },
   { path: 'tour',  component: GuidedtourComponent }
-//   ,
-//   { path: 'detail/:id', component: HeroDetailComponent },
-//   { path: 'heroes',     component: HeroesComponent }
 ];
  
 @NgModule({
