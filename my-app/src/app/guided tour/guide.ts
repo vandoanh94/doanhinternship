671069efeddad0a0d32327313dda60fe247822f3
ORIENTSOFTWARE\nvdoanh <vandoanh94@gmail.com>
2017-09-25 18:31:07 +0700

    update last day

--- my-app/src/app/guided tour/guide.ts
+++ my-app/src/app/guided tour/guide.ts
@@ -1,5 +1,6 @@
 export class Guide {
     id: number;
+    title:string;
     image: string;
     detail:string;
   }
--- my-app/src/app/guided tour/guided_tour.component.ts
+++ my-app/src/app/guided tour/guided_tour.component.ts
@@ -9,19 +9,49 @@ import { GUIDES } from "./mock-guides";
   styleUrls: ['./guided_tour.component.css']
 })
 export class GuidedtourComponent {
-  guides : Guide[];
-  title = 'guided_tour';
   mdTitle = 'Welcome to Trend Monitor';
-  mdBodyTitle = "";
-  myEvent(event) {
-    console.log(event);
-  }
-  onClickNext()
-  {
+  guides: Guide[] = GUIDES;
+  guideSelect = this.guides[0];
+  guideButtonSelect = this.guideSelect.title;
+  
+  i = 0;
 
+  onSelect(idGuide){
+    this.i==idGuide;
+    this.guideSelect = this.guides[idGuide];
+    this.guideButtonSelect = this.guideSelect.title;
+    if (idGuide > 0) {
+      this.mdTitle = "The Tour Guide";
+    }
+    if (idGuide == 0) {
+      this.mdTitle = "Welcome to Trend Monitor";
+    }
   }
-  onClickBack()
-  {
-    
+  onClickNext() {
+    let ii;
+    if (this.i < this.guides.length - 1) {
+      this.i++;
+      ii = this.i;
+      this.guideSelect = this.guides[ii];
+      this.guideButtonSelect = this.guideSelect.title;
+    }
+    if (ii > 0) {
+      this.mdTitle = "The Tour Guide";
+    }
+  }
+  onClickPrevious() {
+    let ii;
+    if (this.i > 0) {
+      this.i--;
+      ii = this.i;
+      this.guideSelect = this.guides[ii];
+      this.guideButtonSelect = this.guideSelect.title;
+    }
+    if (ii > 0) {
+      this.mdTitle = "The Tour Guide";
+    }
+    if (ii == 0) {
+      this.mdTitle = "Welcome to Trend Monitor";
+    }
   }
 }
